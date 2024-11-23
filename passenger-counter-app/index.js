d0d69bc46462ce10357b36b3363a0756d3615968
Suresh Baskar <38977541+vagaisuresh@users.noreply.github.com>
2024-11-23 18:04:25 +0530

    App completion

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,7 +1,16 @@
 let count = 0;
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 
 function increment() {
-    count = count + 1;
-    countEl.innerText = count;
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countText = count + " - ";
+    saveEl.textContent += countText;
+
+    count = 0;
+    countEl.textContent = count;
 }
