a2d603618ee2ce34bbc9c8f59462cd4f1d05be53
Suresh Baskar <38977541+vagaisuresh@users.noreply.github.com>
2024-12-05 17:58:48 +0530

    Calculator

---
+++ calculator/index.js
@@ -0,0 +1,36 @@
+let num1 = 8;
+let num2 = 2;
+
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+let resultElement = document.getElementById("sum-el");
+let result = 0;
+
+function add() {
+    result = num1 + num2;
+    resultElement.textContent = "Sum: " + result;
+}
+
+function subtract() {
+    result = num1 - num2;
+    resultElement.textContent = "Sum: " + result;
+}
+
+function devide() {
+    result = num1 / num2;
+    resultElement.textContent = "Sum: " + result;
+}
+
+function multiply() {
+    result = num1 * num2;
+    resultElement.textContent = "Sum: " + result;
+}
